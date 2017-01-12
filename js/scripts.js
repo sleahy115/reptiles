5e623a4cdcda3bdc933e7fad585440351a75488c
sarah leahy <sarah leahy>
2017-01-12 12:16:43 -0700

    add toggle function

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#snake-heading").click(function(){
+    $("#snakes").toggle();
+  });
+  $("#turtle-heading").click(function(){
+    $("#turtles").toggle();
+  });
+});
